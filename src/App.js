f28a3eec23b40b633511fb28a28dbd852e8accce
Jooka <mr.jooka@gmail.com>
2021-05-04 20:05:55 +0900

    ing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// import axios from "axios";
+// import React, { useState } from "react";
 
-function App() {
+// const App = () => {
+//   const [data, setData] = useState(null);
+
+//   // const onClick = () => {
+//   //   axios.get("https://jsonplaceholder.typicode.com/todos/1").then((response) => {
+//   //     setData(response.data);
+//   //   });
+//   // };
+
+//   const onClick = async () => {
+//     try {
+//       const response = await axios.get("https://newsapi.org/v2/top-headlines?country=kr&category=entertainment&apiKey=05a1ae07151043e5acb1d0a8eaffc544");
+//       setData(response.data);
+//     } catch (e) {
+//       console.log(e);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={onClick}>불러오기</button>
+//       </div>
+//       {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+import NewsList from "./components/NewsList";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewsList />
     </div>
   );
-}
+};
 
 export default App;
