457e54b28004f39a47ef1a28b834bc3f3805a1e5
Jooka <mr.jooka@gmail.com>
2021-05-05 20:01:16 +0900

    카테고리 생성 & 라우터 적용 & usePromise 커스텀 Hook 적용

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 const categories = [
@@ -43,7 +44,8 @@ const CategoriesBlock = styled.div`
   }
 `;
 
-const Category = styled.div`
+// const Category = styled.div`
+const Category = styled(NavLink)`
   font-size: 1.125rem;
   cursor: pointer;
   white-space: pre;
@@ -55,7 +57,7 @@ const Category = styled.div`
     color: #495057;
   }
 
-  ${(props) =>
+  /* ${(props) =>
     props.active &&
     css`
       font-weight: 600;
@@ -64,7 +66,15 @@ const Category = styled.div`
       &::hover {
         color: #3bc9db;
       }
-    `}
+    `} */
+  &.active {
+    font-weight: 600;
+    border-bottom: 2px solid #22b8cf;
+    color: #22b8cf;
+    &::hover {
+      color: #3bc9db;
+    }
+  }
 
   & + & {
     margin-left: 1rem;
@@ -75,7 +85,14 @@ const Categories = ({ onSelect, category }) => {
   return (
     <CategoriesBlock>
       {categories.map((c) => (
-        <Category key={c.name} active={category === c.name} onClick={() => onSelect(c.name)}>
+        <Category
+          key={c.name}
+          // active={category === c.name}
+          // onClick={() => onSelect(c.name)}
+          activeClassName="active"
+          exact={c.name === "all"}
+          to={c.name === "all" ? "/" : `/${c.name}`}
+        >
           {c.text}
         </Category>
       ))}
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,7 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from "styled-components";
+import axios from "axios";
+import usePromise from "../lib/usePromise";
 import NewsItem from "./NewsItem";
 
 const NewsListBlock = styled.div`
@@ -17,31 +18,25 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === "all" ? "" : `&category=${category}`;
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=05a1ae07151043e5acb1d0a8eaffc544`);
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === "all" ? "" : `&category=&${category}`;
+    return axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=05a1ae07151043e5acb1d0a8eaffc544`);
   }, [category]);
 
   if (loading) {
-    return <NewsListBlock>대기 중...</NewsListBlock>;
+    return <NewsListBlock>대기 중 ...</NewsListBlock>;
   }
 
-  if (!articles) {
+  if (!response) {
     return null;
   }
+
+  if (error) {
+    return <NewsListBlock>에러 발생!!</NewsListBlock>;
+  }
+
+  // response 값이 유효할 때
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
@@ -52,3 +47,58 @@ const NewsList = ({ category }) => {
 };
 
 export default NewsList;
+
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
+// import styled from "styled-components";
+// import NewsItem from "./NewsItem";
+
+// const NewsListBlock = styled.div`
+//   box-sizing: border-box;
+//   padding-bottom: 3rem;
+//   width: 768px;
+//   margin: 0 auto;
+//   margin-top: 2rem;
+//   @media screen and (max-width: 768px) {
+//     width: 100%;
+//     padding-left: 1rem;
+//     padding-right: 1rem;
+//   }
+// `;
+
+// const NewsList = ({ category }) => {
+//   const [articles, setArticles] = useState(null);
+//   const [loading, setLoading] = useState(false);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       setLoading(true);
+//       try {
+//         const query = category === "all" ? "" : `&category=${category}`;
+//         const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=05a1ae07151043e5acb1d0a8eaffc544`);
+//         setArticles(response.data.articles);
+//       } catch (e) {
+//         console.log(e);
+//       }
+//       setLoading(false);
+//     };
+//     fetchData();
+//   }, [category]);
+
+//   if (loading) {
+//     return <NewsListBlock>대기 중...</NewsListBlock>;
+//   }
+
+//   if (!articles) {
+//     return null;
+//   }
+//   return (
+//     <NewsListBlock>
+//       {articles.map((article) => (
+//         <NewsItem key={article.url} article={article} />
+//       ))}
+//     </NewsListBlock>
+//   );
+// };
+
+// export default NewsList;
